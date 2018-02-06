8a536d00f906a1b283f0e5a0da1433b345631859
Brandon Ferguson <syntaxic@gmail.com>
2018-02-06 18:58:24 -0500

    Working (and failing) on adding sticky header

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var mn = $(".main-nav");
 
 $(window).scroll(function() {
-   if( $(this).scrollTop () > 250px ) {
-    mn.addClass("main.nav.scrolled")
-   } 
+    if( $(this).scrollTop() > 250 ) {
+    mn.addClass("main-nav-scrolled");
+    } 
     else {
         mn.removeClass("main.nav.scrolled");
     } 
