913c0233ef8ca71cca04999c2be8f879b55bece1
Brandon Ferguson <syntaxic@gmail.com>
2018-02-06 19:55:55 -0500

    Progress kind of made.  Sticks, doesn't unstick

--- js/script.js
+++ js/script.js
@@ -2,11 +2,9 @@ var mn = $(".main-nav");
 
 $(window).scroll(function() {
     if( $(this).scrollTop() > 250 ) {
-    mn.addClass("main-nav-scrolled");
+        mn.addClass("main-nav-scrolled");
     } 
     else {
         mn.removeClass("main.nav.scrolled");
     } 
 });
-
-alert("I work!");
