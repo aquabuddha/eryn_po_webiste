a06b4e61aa119c278b2934054843d3acbe5179e1
Brandon Ferguson <syntaxic@gmail.com>
2018-02-17 16:51:03 -0500

    Working on mobile navigation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 var mn = $(".main-nav");
 
 
+// Open mobile nav
+function openNav() {
+    document.getElementById("mobile-nav").style.width = "100%";
+}
+
+// Close mobile nav
+function closeNav() {
+    document.getElementById("mobile-nav").style.width = "0%";
+}
+
+// JQuery Sticky Nav for non-mobile
 $(window).scroll(function() {
     if( $(this).scrollTop() > 225 ) {
         mn.addClass("main-nav-scrolled");
