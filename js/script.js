368da8309af9d92d3b6268ee64920cfe7da56374
Brandon Ferguson <syntaxic@gmail.com>
2018-02-19 18:42:10 -0500

    Mobile navigation bugs fixed

--- js/script.js
+++ js/script.js
@@ -7,12 +7,40 @@ var mobile = $("#mobile-nav");
 // Open mobile nav
 function openNav() {
     document.getElementById("mobile-nav").style.width = "100%";
+    //adds slide effect when opened
+    document.getElementById("mobile-nav").classList.add("slide");
 }
 
 // Close mobile nav
 function closeNav() {
     document.getElementById("mobile-nav").style.width = "0%";
 }
+
+// need to wait for document to finish loading before implimenting
+$(document).ready(function() {
+    // JQuery for Mobile Nav
+    function checkWidth() {
+        var windowsize = $window.width();
+        //If window's width is more than 440px, then add .sidenav class to mobile-nav ID
+        if (windowsize < 769 ) {
+            mobile.width("0%");
+            mobile.addClass("sidenav");
+        }
+        //If window's width is less than 440px, then remove .sidenav class from mobile-nav ID
+        else {
+            mobile.removeClass("sidenav");
+            mobile.removeClass("slide");
+            mobile.width("100%");
+        }
+    };
+    
+    //Check width on load
+    checkWidth();
+    
+    //Check width on resize
+    $window.resize(checkWidth);
+    });
+    
 
 // JQuery Sticky Nav for non-mobile
 $window.scroll(function() {
@@ -27,26 +55,3 @@ $window.scroll(function() {
 });
 
 
-// need to wait for document to finish loading before implimenting
-$(document).ready(function() {
-// JQuery for Mobile Nav
-function checkWidth() {
-    var windowsize = $window.width();
-    //If window's width is more than 440px, then add .sidenav class to mobile-nav ID
-    if (windowsize < 600) {
-        mobile.width("0%");
-        mobile.addClass("sidenav");
-    }
-    //If window's width is less than 440px, then remove .sidenav class from mobile-nav ID
-    else {
-        mobile.removeClass("sidenav");
-        mobile.width("100%");
-    }
-};
-
-//Check width on load
-checkWidth();
-
-//Check width on resize
-$window.resize(checkWidth);
-});
